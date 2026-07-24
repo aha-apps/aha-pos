11fba36a73f3534c0bae9b434867ad02b04e0a73
Angel Hdz Aguilera <84angel.hdz@gmail.com>
2026-07-24 03:43:16 -0400

    debug: console.trace para navigate corte/devoluciones

--- core/app.js
+++ core/app.js
@@ -75,6 +75,7 @@ window.appRouter = {
 
   navigate(id, params, replace) {
     console.log('[router] navigate(' + id + ') navigatingTo=' + this._navigatingTo + ' current=' + (this._current ? this._current.id : 'null') + ' actual=' + Alpine.store('app').moduloActual);
+    if (id === 'corte' || id === 'devoluciones') console.trace('[router] stack for ' + id);
     params = params || '';
     replace = replace || false;
     var store = Alpine.store('app');
